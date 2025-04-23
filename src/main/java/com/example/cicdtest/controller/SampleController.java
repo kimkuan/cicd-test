2f93a5bb30b0110c9dd946c1ade39b9edccb7221
kimkuan <zxd9857@gmail.com>
2025-04-23 18:31:00 +0900

    change application code

---
+++ src/main/java/com/example/cicdtest/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.example.cicdtest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/version")
+    public String getVersion() {
+        return "0.0.1";
+    }
+}
